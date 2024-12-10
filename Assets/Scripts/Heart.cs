88747b46c926e1795372b3dc27dc73c43ad99c63
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-12-10 17:50:55 +0200

    Update Heart.cs

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -4,24 +4,25 @@ public class Heart : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Speed of rotation in degrees per second.")]
-    public float rotationSpeed = 100f;
-    
+    private float rotationSpeed = 100f;
+
+    [SerializeField]
     [Tooltip("Axis of rotation.")]
-    public Vector3 rotationAxis = Vector3.up;
+    private Vector3 rotationAxis = Vector3.up;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotationAxis,rotationSpeed*Time.deltaTime);
+        // Rotate the heart around the specified axis
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player" && enabled)
+        if (other.CompareTag("Player") && enabled)
         {
-    
-            Destroy(this.gameObject); // Destroy the other object (e.g., bullet)
-         
+            // Destroy this object when the player collides with it
+            Destroy(gameObject);
         }
     }
 }
