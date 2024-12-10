acd74b297543bddf2d28bccb73590fb59a80f3df
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-12-10 17:45:38 +0200

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
-{ 
-    private Vector3 offset=new Vector3 (0f, 3f, -10f);
+{
+    private Vector3 offset = new Vector3(0f, 3f, -10f);
     private float smoothTime = 0.25f;
-    private Vector3 velocity= Vector3.zero;
-    
+    private Vector3 velocity = Vector3.zero;
+
     [SerializeField]
     private Transform target;
 
@@ -13,6 +13,6 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition,ref velocity ,smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
