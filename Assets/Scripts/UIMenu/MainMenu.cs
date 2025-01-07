cfdc4735be88fe72d055650b2dffcb83e4b76227
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2025-01-07 22:28:40 +0200

    Update MainMenu.cs

--- Assets/Scripts/UIMenu/MainMenu.cs
+++ Assets/Scripts/UIMenu/MainMenu.cs
@@ -3,16 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   
-   public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene("Game");
     }
+
     public void ABOUT()
     {
         SceneManager.LoadScene("About");
-
     }
+
     public void BACK_TOMENU()
     {
         SceneManager.LoadScene("Menu");
