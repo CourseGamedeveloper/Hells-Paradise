bccab0d341982ea724b711d5a10903191da63b86
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2025-02-04 19:18:14 +0200

    Update Prison.cs

--- Assets/Scripts/Prison/Prison.cs
+++ Assets/Scripts/Prison/Prison.cs
@@ -1,44 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Manages the prison mechanics, including detecting key possession and opening the prison door.
+/// </summary>
 public class Prison : MonoBehaviour
 {
-    private bool IsHavekey;
+    private bool IsHaveKey;
     public GameObject PrisonDoor;
     private GameManager Manager;
-    void Start()
+
+    private void Start()
     {
-        Manager=FindAnyObjectByType<GameManager>();
-        IsHavekey = false;
+        Manager = FindAnyObjectByType<GameManager>();
+        IsHaveKey = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Sets the key possession status to true.
+    /// </summary>
+    public void SetIsHaveKey()
     {
-        
+        IsHaveKey = true;
+        Debug.Log("The player has the key: " + IsHaveKey);
     }
-    public void setIsHaveKey()
-    {
-        IsHavekey = true;
-        Debug.Log("the player Have key: " + IsHavekey);
-    }
-    private void OpenPrisonDoor() 
+
+    /// <summary>
+    /// Opens the prison door if the player has the key.
+    /// </summary>
+    private void OpenPrisonDoor()
     {
         Vector3 currentPosition = PrisonDoor.transform.position;
         PrisonDoor.transform.position = new Vector3(currentPosition.x, currentPosition.y + 3f, currentPosition.z);
         Manager.OpenGate();
-
     }
-    
+
+    /// <summary>
+    /// Detects if the player enters the trigger area and opens the door if they have the key.
+    /// </summary>
+    /// <param name="other">The collider that enters the trigger.</param>
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && IsHaveKey)
         {
-            if (IsHavekey)
-            {
-                Debug.Log("the door is open");
-                OpenPrisonDoor();
-            }
+            Debug.Log("The door is open");
+            OpenPrisonDoor();
         }
     }
 }
