79892c47a280b7364a2f071a2b4424a070d3e8c8
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2025-02-04 19:12:06 +0200

    Update PlayerAttack.cs

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
 
+/// <summary>
+/// Manages player attack actions, including animations and sound effects.
+/// </summary>
 public class PlayerAttack : MonoBehaviour
 {
     private Animator animator;
-    private bool isAttack=false;
+    private bool isAttack = false;
 
-    [Tooltip("audio source for the sowrd sound effect")]
+    [Tooltip("Audio source for the sword sound effect.")]
     public AudioSource AudioSwordEffect;
 
-    [Tooltip("sound effect for the sowrd ")]
+    [Tooltip("Sound effect for the sword.")]
     public AudioClip SowrdSoundEffect;
-   
 
     private void Awake()
     {
-        
         animator = GetComponent<Animator>();
     }
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
             isAttack = true;
-            handleAttack(0);
+            HandleAttack(0);
         }
         if (Input.GetMouseButtonDown(0))
         {
-            handleAttack(1);
+            HandleAttack(1);
         }
         if (Input.GetMouseButtonDown(1))
         { 
-            handleAttack(2);
+            HandleAttack(2);
         }
-        isAttack = false;
-        animator.SetBool("isAttack",isAttack);
-    }
-    private void handleAttack(int attackIndex)
-    {
-      
 
+        isAttack = false;
+        animator.SetBool("isAttack", isAttack);
+    }
+
+    /// <summary>
+    /// Handles different attack animations based on the attack index.
+    /// </summary>
+    /// <param name="attackIndex">The attack type index.</param>
+    private void HandleAttack(int attackIndex)
+    {
         switch (attackIndex) 
         { 
             case 0:
@@ -49,20 +54,20 @@ public class PlayerAttack : MonoBehaviour
             case 1:
                 animator.SetTrigger("sword1");
                 animator.SetTrigger("sword2");
-                HandleSowrdSoundEffect();
-
+                HandleSwordSoundEffect();
                 break;
             case 2:
                 animator.SetTrigger("slash1");
-                HandleSowrdSoundEffect();
+                HandleSwordSoundEffect();
                 break;
         }
     }
-    private void HandleSowrdSoundEffect()
+
+    /// <summary>
+    /// Plays the sword attack sound effect.
+    /// </summary>
+    private void HandleSwordSoundEffect()
     {  
         AudioSwordEffect.PlayOneShot(SowrdSoundEffect); 
     }
-    
-   
-
 }
