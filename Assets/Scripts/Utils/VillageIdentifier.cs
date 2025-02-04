de5f7da8811d3fc0385af8d1dc4125d3191adc0e
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2025-02-04 19:23:07 +0200

    Update VillageIdentifier.cs

--- Assets/Scripts/Utils/VillageIdentifier.cs
+++ Assets/Scripts/Utils/VillageIdentifier.cs
@@ -1,6 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 
+/// <summary>
+/// Identifies a village by assigning it a unique ID.
+/// </summary>
 public class VillageIdentifier : MonoBehaviour
 {
-    public int villageId; // מספר הכפר
+    /// <summary>
+    /// The unique ID of the village.
+    /// </summary>
+    public int villageId;
 }
